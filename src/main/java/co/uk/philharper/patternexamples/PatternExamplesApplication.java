00ebbda231986504df739d33c36cfb88bf9e4deb
Phil Harper <philiph@motabilityoperations.co.uk>
2021-02-06 16:21:12 +0000

    Init project

---
+++ src/main/java/co/uk/philharper/patternexamples/PatternExamplesApplication.java
@@ -0,0 +1,13 @@
+package co.uk.philharper.patternexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/philharper/patternexamples/PatternExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.philharper.patternexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
